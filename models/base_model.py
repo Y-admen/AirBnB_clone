c39dfdefae0afc4aaa661ee90cb3d213df8c6a9f
Y-admen <yassminash305@gmail.com>
2024-03-06 06:33:14 +0200

    Base

---
+++ models/base_model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Base class model"""
+from datetime import datetime
+from uuid import uuid4
+
+class BaseModel:
+    """Base class for models"""
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """print representation"""
+        return (f"[{self.__class__.__name__}] {self.id} {self.__dict__}")
+
+    def save(self):
+        """update modification time"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """convert to dictionary"""
+        dict_cp = self.__dict__.copy()
+        dict_cp['created_at'] = self.created_at.isoformat()
+        dict_cp['updated_at'] = self.updated_at.isoformat()
+        dict_cp['__class__'] = self.__class__.__name__
+        return dict_cp
