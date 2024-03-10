5b5d6734fc169fda9959ae63dff373f9da788894
MamdouhEnahla <mamdouh57118@gmail.com>
2024-03-10 03:48:52 +0200

    edit-user...

--- models/user.py
+++ models/user.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
+"""
+User model class that inherits from BaseModel.
+
+Attributes:
+  - email (str): Email address of the user
+  - password (str): Hashed password for the user
+  - first_name (str): First name of the user
+  - last_name (str): Last name of the user
+
+"""
 from base_model import BaseModel
-from engine.file_storage import FileStorage
 
 class User(BaseModel):
+    """
+    A User class
+    """
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+  
+    def __init__(self, *args, **kwargs):
+        """ Initial user attributes"""
+        super().__init__(*args, **kwargs)
+
