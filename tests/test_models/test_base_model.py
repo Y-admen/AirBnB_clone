ba00a0e217b76ad3526a00fb536783b6e4130444
MamdouhEnahla <mamdouh57118@gmail.com>
2024-03-12 08:49:18 +0200

    update-test-base...

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""Tests for BaseModel"""
 import unittest
 import uuid
 from models.base_model import BaseModel
@@ -5,40 +7,49 @@ from models.engine.file_storage import FileStorage
 
 
 class TestBaseModel(unittest.TestCase):
-
+    """This class represents the test class for BaseModel"""
     def setUp(self):
-        self.BaseModel = BaseModel()
-        super().setUp()
+        """
+        Initialize class instance
+        """
+        self.file_storage = FileStorage()
+        self.base_model = BaseModel()
 
     def tearDown(self):
-        # Clean up code
-        ... 
+        """
+        Remove class instance
+        """
+        FileStorage._FileStorage__objects = {}
 
     def test_id_uniqueness(self):
+        """Test that id is unique for each instance"""
         id1 = self.BaseModel.id
         id2 = BaseModel().id
         self.assertNotEqual(id1, id2)
 
-    def test_save_method_updates_updated_at(self):
+    def test_save(self):
+        """Test that save method saves the object to the storage"""
         prev_updated_at = self.BaseModel.updated_at
         self.BaseModel.save()
         self.assertNotEqual(prev_updated_at, self.BaseModel.updated_at)
 
-    def test_to_dict_method_returns_dictionary(self):
+    def test_to_dict(self):
+        """Test that to_dict returns a dictionary"""
         BaseModel_dict = self.BaseModel.to_dict()
         self.assertIsInstance(BaseModel_dict, dict)
 
     def test_instance_str(self):
+        """Test that instance string representation is correct"""
         inst = self.BaseModel
         id = inst.id
         string = "[{}] ({}) {}".format(inst.__class__.__name__,
                                        id, inst.__dict__)
         self.assertEqual(inst.__str__(), string)
 
-    def test_str_method_returns_string_representation(self):
+    def test_str(self):
+        """Test that string representation is correct"""
         string_representation = str(self.BaseModel)
         self.assertIsInstance(string_representation, str)
-
 
 
 if __name__ == '__main__':
